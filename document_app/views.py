57fb2775d1a5e7052f0a633199990a4f8ae1d10c
Dakota Madden-Fong <maddenfong@gmail.com>
2022-01-25 12:25:40 -0800

    added email validation check

--- document_app/views.py
+++ document_app/views.py
@@ -1,4 +1,5 @@
 import hashlib, json, datetime, requests
+from email_validator import validate_email, EmailNotValidError
 
 from django.conf import settings
 from django.http import JsonResponse, HttpResponse
@@ -27,18 +28,39 @@ def publishDocument(request):
     # extract data from request body
     data = json.loads(request.body)
 
+    # verify that request contains needed fields
+    if ('text' not in data or not data['text']
+    or 'creator' not in data or not data['creator']
+    or 'recaptchaToken' not in data or not data['recaptchaToken']):
+        return HttpResponse(
+            '400 Required Field Missing or Empty',
+            status = 400
+        )
+
     # verify recaptcha token with key
-    r = requests.post(settings.RECAPTCHA_V2_URL, data={
+    r = requests.post(settings.RECAPTCHA_V2_URL, data = {
         'secret': settings.RECAPTCHA_V2_SECRET_KEY,
         'response': data['recaptchaToken'],
     })
-    if (not r.json()['success']):
-        return HttpResponse('401 Unauthorized', status=401)
+    if (not 'success' in r.json() or not r.json()['success']):
+        return HttpResponse(
+            '401 Unauthorized',
+            status = 401
+        )
+
+    # verify that email address is valid
+    try:
+        email = validate_email(data['creator']).email
+    except EmailNotValidError as e:
+        return HttpResponse(
+            '400 Invalid Email',
+            status = 400
+        )
 
     # fill hash with text, creator email and current time
     hash = hashlib.shake_256()
     hash.update(data['text'].encode())
-    hash.update(data['creator'].encode())
+    hash.update(email.encode())
     hash.update(str(datetime.datetime.now()).encode())
 
     # create the key with length based on model field length
@@ -47,7 +69,7 @@ def publishDocument(request):
 
     models.Document.objects.create(
         markdown_text = data['text'],
-        creator = data['creator'],
+        creator = email,
         key = key
     )
 
