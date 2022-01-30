a7b7787b37bcfa9770b58bdeb65e1994d8cdd12d
Dakota Madden-Fong <maddenfong@gmail.com>
2022-01-29 17:02:07 -0800

    added first tests

--- document_app/tests/test_views.py
+++ document_app/tests/test_views.py
@@ -1,8 +1,14 @@
+import json
+
 from django.test import TestCase
+from django.urls import reverse
+
 from document_app import models
+
 
 class ViewsTestCase(TestCase):
 
+    # start with a document in the db already
     def setUp(self):
         models.Document.objects.create(
             markdown_text = '## This is my document',
@@ -10,5 +16,32 @@ class ViewsTestCase(TestCase):
             key = 'aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa',
         )
 
-    def test_getDocument_view(self):
-        self.assertEqual(True, True)
+    # tests document retrieval
+    def test_getDocument_exists(self):
+        response = self.client.get(reverse(
+                'getDocument', 
+                kwargs = {
+                    'key': 'aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa',
+                },
+        ))
+        self.assertEqual(
+            response.status_code, 
+            200
+        )
+        self.assertEqual(
+            json.loads(response.content)['text'], 
+            '## This is my document'
+        )
+
+    # test 404 when document does not exist
+    def test_getDocument_missing(self):
+        response = self.client.get(reverse(
+                'getDocument', 
+                kwargs = {
+                    'key': 'aaaaaaaa-aaaa-aaaa-aaaa-bbbbbbbbbbbb',
+                },
+        ))
+        self.assertEqual(
+            response.status_code, 
+            404
+        )
